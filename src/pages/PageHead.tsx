06a8bad524c383832d897b38ecf427cb5367d31b
baginskijakub <baginskijakub@icloud.com>
2022-10-04 15:20:22 +0200

    React-helmet for better SEO

--- src/pages/PageHead.tsx
+++ src/pages/PageHead.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './pages.css'
+import {Helmet} from "react-helmet"
+import ggIcon from '../images/icons/gg_logo.png'
 
 interface Props{
     title: string
@@ -8,6 +10,10 @@ interface Props{
 export const PageHead:React.FC<Props> = ({title, text}) => {
     return (
         <div className="page-head-wrapper">
+            <Helmet>
+                <title>{title}</title>
+                <link rel="icon" href={ggIcon}></link>
+            </Helmet>
             <h2>{title}</h2>
             {text !== undefined && <p className="body">{text}</p>}
         </div>
