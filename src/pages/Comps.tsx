faa932e20af30471c996a1ed05bb26845b89b2bf
baginskijakub <baginskijakub@icloud.com>
2022-10-18 22:51:03 +0200

    Canonical fixed in Comp page

--- src/pages/Comps.tsx
+++ src/pages/Comps.tsx
@@ -128,7 +128,7 @@ export const Comps = () => {
             <PageHead 
                 title="TFT Meta Team Comps"
                 text="Teamfight Tactics data-driven composition tier list based on current meta"
-                canonical="/comps"
+                canonical=""
                 />
             <div className="sort-navigation-container">
                 <Dropdown 
